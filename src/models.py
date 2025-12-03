4a79422aeb0bbf2982abe5399f6e45f93f5463f0
Vayun Naidoo <132143990+vayun-naidoo@users.noreply.github.com>
2025-12-03 17:50:22 +0200

    changed updater

--- src/models.py
+++ src/models.py
@@ -2,41 +2,36 @@ import json
 
 # JSON decoder
 def import_teams(json_file):
+    team_list = []
     try:
         with open(json_file, 'r') as f:
             teams = json.load(f)
         for team_name, wr_points in teams.items():
             team = Team(team_name, wr_points)
-            #print(team)
-    
+            team_list.append(team)
+            
     except FileNotFoundError:
         print(f"File {json_file} not found.")
     except json.JSONDecodeError:
         print(f"Error decoding JSON from file {json_file}.")
+    
+    return team_list
 
-
-# Consider the team as read from WR rankings
+# Consider the team as read from pool stage with additional stats
 class Team:
-    def __init__(self, name = '', wr_ranking = 30.00):
+    def __init__(self, name = '', wr_ranking = 30.00, pool = None, pool_points = None, points_for = None, points_against = None, tries_for = None, tries_against = None):
         self.name = name
         self.wr_ranking = wr_ranking
-    
-    def __str__(self):
-        return f"{self.name} (WR Points: {self.wr_ranking})"
-
-# Consider the team as read from group stage with additional stats
-class Group_Team(Team):
-    def __init__(self, name, wr_ranking, group_points = None, points_for = None, points_against = None, tries_for = None, tries_against = None):
-        super().__init__(name, wr_ranking)
-        self.group_points = group_points # This is the actual W/L points in the group stage
+        self.pool = pool
+        self.pool_points = pool_points # This is the actual points value for the pool stage
         self.points_for = points_for
         self.points_against = points_against
         self.tries_for = tries_for
         self.tries_against = tries_against
     
     def __str__(self):
-        return f"{self.name}  (Group Points: {self.group_points})"
-
+        return f"{self.name}:\n- WR: {self.wr_ranking} points\n- Pool: {self.pool}\n- Pool Points: {self.pool_points}\n- Points For: {self.points_for}\n- Points Against: {self.points_against}\n- Tries For: {self.tries_for}\n- Tries Against: {self.tries_against}"
+    
 # Match object to hold match information
 class Match:
     def __init__(self, team1, team2, team1_score = None, team1_tries = None, team2_tries = None, team2_score = None):
@@ -51,6 +46,7 @@ class Match:
         # Placeholder for match simulation logic
         pass
 
+teams = import_teams('data/teams.json')
 
-
-import_teams('data/teams.json')
+for t in teams:
+    print(t)
--- src/updater.py
+++ src/updater.py
@@ -5,6 +5,46 @@ import json
 # Prevent website blocking by setting a user-agent for browser
 headers ={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
 
+confirmed_teams = {
+    # Pool A
+    'New Zealand': 'A',
+    'Australia' : 'A',
+    'Chile': 'A',
+    'Hong Kong': 'A',
+
+    # Pool B
+    'South Africa': 'B',
+    'Italy': 'B',
+    'Georgia': 'B',
+    'Romania': 'B',
+    
+    # Pool C
+    'Argentina': 'C',
+    'Fiji': 'C',
+    'Spain': 'C',
+    'Canada': 'C',
+
+    # Pool D
+    'Ireland': 'D',
+    'Scotland': 'D',
+    'Uruguay': 'D',
+    'Portugal': 'D',
+
+    # Pool E
+    'France': 'E',
+    'Japan': 'E',
+    'United States': 'E',
+    'Samoa': 'E',
+
+    # Pool F
+    'England': 'F',
+    'Wales': 'F',
+    'Tonga': 'F',
+    'Zimbabwe': 'F',
+    
+}
+
+
 # Get response from the Wikipedia page
 url = 'https://en.wikipedia.org/wiki/World_Rugby_Rankings'
 response = requests.get(url, headers=headers)
@@ -20,7 +60,7 @@ soup = BeautifulSoup(response.text, 'html.parser')
 # Get the instance of the rankings table
 table = soup.find('table', class_='wikitable floatright sticky-header')
 
-scraped_data = {}
+scraped_data = {'teams' : []}
 
 rows = table.find_all('tr')[1:]  # Skip the header row
 
@@ -29,10 +69,14 @@ for row in rows:
     if len(cols) > 2:
         # Get team information
         team_name = cols[1].get_text(strip=True)
-        points = cols[2].get_text(strip=True)[1:]         
-        # Save it to our dictionary
-        scraped_data[team_name] = float(points)
+        wr_points = cols[2].get_text(strip=True)[1:]
 
+        if team_name in confirmed_teams:
+            # Save it to our dictionary
+            scraped_data['teams'].append({'team_name':team_name, 
+                                          'wr_points':float(wr_points), 
+                                          'pool': confirmed_teams[team_name]})
+        
 # Save the scraped data to a JSON file
 with open('data/teams.json', 'w') as f:
-    json.dump(scraped_data, f, indent=4)
+   json.dump(scraped_data, f, indent=4)
